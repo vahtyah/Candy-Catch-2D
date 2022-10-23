0a5671c80a1bd231fdd928c50a22ed9a3cdc8938
MAI VAN TUAN <86241573+8268826@users.noreply.github.com>
2022-10-23 23:51:07 +0300

    Complete game

--- Candy Catch 2D/Assets/Scripts/GameManager.cs
+++ Candy Catch 2D/Assets/Scripts/GameManager.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public GameObject livesHolder;
+    public GameObject gameOverPanel;
+
     int score = 0;
     int lives = 3;
     bool gameOver = false;
@@ -30,8 +34,11 @@ public class GameManager : MonoBehaviour
 
     public void IncrementScore()
     {
-        score++;
-        scoreText.text = score.ToString();
+        if (!gameOver)
+        {
+            score++;
+            scoreText.text = score.ToString();
+        }
     }
 
     public void DecreaseLife()
@@ -40,15 +47,30 @@ public class GameManager : MonoBehaviour
         {
             lives--;
             print(lives);
+            livesHolder.transform.GetChild(lives).gameObject.SetActive(false);
         }
-        else
+        if(lives <= 0)
         {
             gameOver = true;
+            GameOver();
         }
     }
 
     public void GameOver()
     {
+        CandySpawner.instance.StopSpawningScandies();
+        GameObject.Find("Player").GetComponent<PlayerController>().canMove = false;
+        gameOverPanel.SetActive(true);
         print("Game Over!");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
     }
 }
--- Candy Catch 2D/Assets/Scripts/PlayerController.cs
+++ Candy Catch 2D/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    bool canMove = true;
+    public bool canMove = true;
 
     [SerializeField] float moveSpeed;
     [SerializeField] float maxPos;
